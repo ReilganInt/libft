6ca096fb2d44e9d79090afcc797fd8d2d54bf654
Vanna Mormont <vmormont@ga-g5.21-school.ru>
2019-10-24 17:53:54 +0300

    Add ft_strdel prototype to libft.h

--- include/libft.h
+++ include/libft.h
@@ -199,6 +199,7 @@ int					ft_isalpha(int c);
 int					ft_isascii(int c);
 int					ft_isdigit(int c);
 int					ft_isprint(int c);
+void				ft_strdel(char **as);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_strstr(const char *haystack, const char *needle);
